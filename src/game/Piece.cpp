bb46e22a0bfed2d5412bd1b4b79f7913654310fe
Nicolas Winkler <niwinkle@slabhg192.ethz.ch>
2015-11-04 14:38:41 +0100

    added test implementation for Piece

---
+++ src/game/Piece.cpp
@@ -0,0 +1,16 @@
+//
+// file header
+//
+
+#include "Piece.h"
+
+namespace chess {
+
+Piece::~Piece(void)
+{
+}
+
+}
+
+
+
---
+++ src/game/Piece.h
@@ -0,0 +1,32 @@
+//
+// insert file header here
+//
+
+#ifndef CHESS_GAME_PIECE_H_
+#define CHESS_GAME_PIECE_H_
+
+namespace chess {
+
+
+class Piece
+{
+public:
+    virtual ~Piece(void);
+
+    // TODO: add more here
+};
+
+
+class Bishop : public Piece
+{
+public:
+    // TODO: add more here
+};
+
+
+}
+
+
+#endif // CHESS_GAME_PIECE_H_
+
+
